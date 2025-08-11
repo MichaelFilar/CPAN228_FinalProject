a4c1b531aeb4577a2dd368f4cf0a2b539e6cd66f
Michael Filar <michaelfilar@hotmail.com>
2025-08-11 00:01:55 -0400

    authorization/encryption controllers added

---
+++ src/main/java/com/example/FinalProject/service/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+/*package com.example.FinalProject.service;
+
+import com.example.FinalProject.model.User;
+import com.example.FinalProject.repository.UserRepository;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository repo) {
+        this.userRepository = repo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new CustomUserDetails(user);
+    }
+}
+*/
